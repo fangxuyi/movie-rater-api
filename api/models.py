bbd2cf3e3f4277dce1f2f3f62780b59d1c48d1a9
Lisa Yi <62482176+fangxuyi@users.noreply.github.com>
2022-07-03 11:39:46 -0400

    Add project code

---
+++ api/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+
+    def avg_rating(self):
+        sum = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            sum += rating.stars
+        if len(ratings) > 0:
+            return round(sum / len(ratings), 2)
+        else:
+            return None
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+
+    class Meta:
+        unique_together = (('user', 'movie'),)
+        index_together = (('user', 'movie'),)
